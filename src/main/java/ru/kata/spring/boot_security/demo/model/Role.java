a271760f97f1eb554cf144aadee369ce259aa594
Vperedivverh <Vpered.vverh@yandex.ru>
2022-08-31 13:05:20 +0300

    ok

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -1,11 +1,10 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import java.util.Objects;
-@Data
+
 @Entity
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
@@ -16,8 +15,34 @@ public class Role implements GrantedAuthority {
     @Column(name = "name")
     private String name;
 
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role() {
+    }
+
     @Override
     public String getAuthority() {
         return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id && Objects.equals(name, role.name);
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
